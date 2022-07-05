348dc3e957ca970e2f6c3d166c6be7e186b3088c
Héctor Oñate <hectoronari@gmail.com>
2022-07-05 18:45:58 -0500

    data.py and functions.py v1
    data collections and process, avg vol last 14 prices

--- data.py
+++ data.py
@@ -1,4 +1,3 @@
-
 """
 # -- --------------------------------------------------------------------------------------------------- -- #
 # -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
@@ -9,4 +8,46 @@
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
 
-dict_test = {'key_a': 'a', 'key_b': 'b'}
+"""
+1) Descargar order books:
+"""
+# Importar Librerias
+import pandas as pd
+import json 
+import numpy as np
+
+def order_books(file_name):
+    # Opening JSON file
+    f = open(file_name)
+    print(f)
+
+    # Returns JSON object as a dictionary
+    orderbooks_data = json.load(f)
+    ob_data = orderbooks_data['bitfinex']
+
+    # Drop Keys with none values
+    ob_data = {i_key: i_value for i_key,i_value in ob_data.items() if i_value is not None}
+
+    # Convert to DataFrame and rearange columns
+    ob_data = {i_ob: pd.DataFrame(ob_data[i_ob])[['bid_size', 'bid', 'ask', 'ask_size']]
+            if ob_data[i_ob] is not None else None for i_ob in list(ob_data.keys())}
+    return ob_data
+
+# Prueba
+ob_data = order_books('files/orderbooks_05jul21.json')
+#ob_data = order_books('orderbooks_05jul21.json')
+
+
+def public_trades_metrics(pt_data):
+    # -- Cantida de trades publicos que ocurren en 1 hora -- #
+    #pt_data.drop('Unnamed: 0', inplace=True, axis = 1)
+    a = pd.read_csv(pt_data)
+    a.index = pd.to_datetime(a['timestamp']) # Convertir de str a timestamp
+
+    return a
+
+
+
+names = ['Open time','Open','High','Low','Close','Volume','Close time','Quote asset volume','Number of trades','Taker buy base asset volume','Taker buy quote asset volume','Ignore']
+aave = pd.read_csv('files/AAVE/AAVEUSDT-15m-2022-06-01.csv', names = names)
+aave2 = pd.read_csv('files/AAVE/AAVEUSDT-15m-2022-06-02.csv', names = names)
--- functions.py
+++ functions.py
@@ -8,3 +8,29 @@
 # -- repository: YOUR REPOSITORY URL                                                                     -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
+
+import data as dt
+import pandas as pd
+import numpy as np
+
+# ob_ts = list(dt.ob_data.keys())
+# l_ts = [pd.to_datetime(i_ts) for i_ts in ob_ts] # Modificar todos los valores de str a unidades de tiempo
+
+# mid_prices = [(dt.ob_data[ob_ts[i]]['ask'][0] + dt.ob_data[ob_ts[i]]['bid'][0])/2 for i in range(len(ob_ts))]
+
+#n_df = dt.ptm.drop(columns = ['Unnamed: 0', 'timestamp'])
+
+
+
+
+
+
+
+# Leer datos
+aave = dt.aave
+
+# ----------- Promedio del volumen de los ultimos 14 precios: ----------- #
+
+v = np.zeros(len(aave))
+v[:len(aave)-14] = [np.mean(aave.iloc[0:,4][i:i+13]) for i in range(len(aave)-14)]
+avg_volume = pd.DataFrame({'avg_volume':v})
