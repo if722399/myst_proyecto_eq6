c5934d6e47c7ff57b89bc384e7837b19064086e5
Héctor Oñate <hectoronari@gmail.com>
2022-07-06 22:59:29 -0500

    functions
    vwap and pivots functions v1

--- functions.py
+++ functions.py
@@ -10,51 +10,36 @@
 import data as dt
 import pandas as pd
 import numpy as np
-
-# ob_ts = list(dt.ob_data.keys())
-# l_ts = [pd.to_datetime(i_ts) for i_ts in ob_ts] # Modificar todos los valores de str a unidades de tiempo
-
-# mid_prices = [(dt.ob_data[ob_ts[i]]['ask'][0] + dt.ob_data[ob_ts[i]]['bid'][0])/2 for i in range(len(ob_ts))]
-
-#n_df = dt.ptm.drop(columns = ['Unnamed: 0', 'timestamp'])
-
-
-
-
-
-
-
-# Leer datos
-aave = dt.aave
+import pandas_ta as pta
 
 # ----------- Promedio del volumen de los ultimos 14 precios: ----------- #
 
-v = np.zeros(len(aave))
-v[:len(aave)-14] = [np.mean(aave.iloc[0:,4][i:i+13]) for i in range(len(aave)-14)]
-avg_volume = pd.DataFrame({'avg_volume':v})
+# v = np.zeros(len(aave))
+# v[:len(aave)-14] = [np.mean(aave.iloc[0:,4][i:i+13]) for i in range(len(aave)-14)]
+# avg_volume = pd.DataFrame({'avg_volume':v})
 
 # ---------- RSI: -----------# 
 
-diferencias = np.diff(aave.iloc[0:,4][::-1])
+# diferencias = np.diff(aave.iloc[0:,4][::-1])
 
 
-RSI = []
-for i in range(len(diferencias)-14):
-    subidas = []
-    bajadas = []
-    temp = diferencias[i:i+14]
-    for j in temp:
-        if j>0:
-            subidas.append(j)
-        elif j<0:
-            bajadas.append(j)
-        else:
-            None
-        m_subidas = np.mean(subidas)
-        m_bajadas = np.abs(np.mean(bajadas))
+# RSI = []
+# for i in range(len(diferencias)-14):
+#     subidas = []
+#     bajadas = []
+#     temp = diferencias[i:i+14]
+#     for j in temp:
+#         if j>0:
+#             subidas.append(j)
+#         elif j<0:
+#             bajadas.append(j)
+#         else:
+#             None
+#         m_subidas = np.mean(subidas)
+#         m_bajadas = np.abs(np.mean(bajadas))
 
-        RS = (m_subidas/m_bajadas)
-        RSI.append(100 - (100/(1+RS)))
+#         RS = (m_subidas/m_bajadas)
+#         RSI.append(100 - (100/(1+RS)))
 
 # 1) ¿Qué hacer cuando no hay subidas/bajadas? 
 
@@ -70,49 +55,146 @@ for i in range(len(diferencias)-14):
 
 
 
+#------------------------------VWAP--------------------------------------
+def vwap(high, low, close, volume, anchor=None, offset=None, **kwargs):
+    """Indicator: Volume Weighted Average Price (VWAP)"""
+
+    anchor = anchor.upper() if anchor and isinstance(anchor, str) and len(anchor) >= 1 else "D"
+    offset = pta.utils.get_offset(offset)
+    typical_price = pta.hlc3(high=high, low=low, close=close)
+
+    # Calculate Result
+    wp = typical_price * volume
+    vwap  = wp.groupby(wp.index.to_period(anchor)).cumsum()
+    vwap /= volume.groupby(volume.index.to_period(anchor)).cumsum()
+
+    # Offset
+    if offset != 0:
+        vwap = vwap.shift(offset)
+
+    # Handle fills
+    if "fillna" in kwargs:
+        vwap.fillna(kwargs["fillna"], inplace=True)
+    if "fill_method" in kwargs:
+        vwap.fillna(method=kwargs["fill_method"], inplace=True)
+
+    # Name & Category
+    vwap.name = f"VWAP_{anchor}"
+    vwap.category = "overlap"
+
+    return vwap
 
 
-# -------------- Pivot Points -------------- #
+#-------------------------------PIVOTS-----------------------------------
+def pivots(_open, high, low, close, anchor=None, method=None):
 
-# Obtenemos metricas del día 1 de Junio
+    anchor = anchor.upper() if anchor and isinstance(anchor, str) and len(anchor) >= 1 else "D"
+    method_list = ["traditional", "fibonacci", "woodie", "classic", "demark", "camarilla"]
+    method = method if method in method_list else "traditional"
+    date = close.index
 
-def Pivot_points(df) -> dict:
+    freq = pd.infer_freq(date)
+    df = pd.DataFrame(
+        index=date,
+        data={"open": _open.values, "high": high.values, "low": low.values, "close": close.values},
+    )
 
-    '''
-    df: DataFrame que contiene las columnas: [Open,High,Low,Close]
-    
-    '''
+    if freq is not anchor:
+        a = pd.DataFrame()
+        a["open"] = df["open"].resample(anchor).first()
+        a["high"] = df["high"].resample(anchor).max()
+        a["low"] = df["low"].resample(anchor).min()
+        a["close"] = df["close"].resample(anchor).last()
+    else:
+        a = df
 
-    H = df.iloc[0:,[1,2,3,4]].max().max() # Máximo del día anterior
-    L = df.iloc[0:,[1,2,3,4]].min().min() # Mínimo del día anterior
-    C = df['Close'][df['Close time'] == np.max(df['Close time'])].values[0] # Último cierre
+    # Calculate the Pivot Points
+    if method == "traditional":
+        a["p"] = (a.high.values + a.low.values + a.close.values) / 3
 
-    P = (H + L + C)/3 # Pivot point base
-    S1 = (P*2) - H # Primer soporte
-    R1 = (P*2) - L # Primer resistencia 
-    S2 = P - (R1-S1) # Segundo soporte
-    R2 = P + (R1-S1) # Segunda resistencia 
-    S3 = P - (R2-S2) # Tercer soporte
-    R3 = P + (R2-S2) # Tercer resistencia 
+        a["bc"] = (a.high.values + a.low.values ) / 2
+        a["tc"] = (2 * a.p.values) - a.bc.values
+        a["rng"] = abs(a.tc.values-a.bc.values)/a.p.values*100
 
-    return {'Pivot point base':P, 'Primer soporte':S1, 'Primer resistencia':R1, 'Segundo soporte':S2,
-            'Segunda resistencia':R2, 'Tercer soporte':S3, 'Tercer resistencia':R3}
+        a["s1"] = (2 * a.p.values) - a.high.values
+        a["s2"] = a.p.values - (a.high.values - a.low.values)
+        a["s3"] = a.p.values - (a.high.values - a.low.values) * 2
+        a["r1"] = (2 * a.p.values) - a.low.values
+        a["r2"] = a.p.values + (a.high.values - a.low.values)
+        a["r3"] = a.p.values + (a.high.values - a.low.values) * 2
+
+    elif method == "fibonacci":
+        a["p"] = (a.high.values + a.low.values + a.close.values) / 3
+        a["pivot_range"] = a.high.values - a.low.values
+        a["s1"] = a.p.values - 0.382 * a.pivot_range.values
+        a["s2"] = a.p.values - 0.618 * a.pivot_range.values
+        a["s3"] = a.p.values - 1 * a.pivot_range.values
+        a["r1"] = a.p.values + 0.382 * a.pivot_range.values
+        a["r2"] = a.p.values + 0.382 * a.pivot_range.values
+        a["r3"] = a.p.values + 1 * a.pivot_range.values
+        a.drop(["pivot_range"], axis=1, inplace=True)
+
+    elif method == "woodie":
+        a["pivot_range"] = a.high.values - a.low.values
+        a["p"] = (a.high.values + a.low.values + a.open.values * 2) / 4
+        a["s1"] = a.p.values * 2 - a.high.values
+        a["s2"] = a.p.values - 1 * a.pivot_range.values
+        a["s3"] = a.high.values + 2 * (a.p.values - a.low.values)
+        a["s4"] = a.s3 - a.p.values
+        a["r1"] = a.p.values * 2 - a.low.values
+        a["r2"] = a.p.values + 1 * a.pivot_range.values
+        a["r3"] = a.low.values - 2 * (a.high.values - a.p.values)
+        a["r4"] = a.r3 + a.p.values
+        a.drop(["pivot_range"], axis=1, inplace=True)
+
+    elif method == "classic":
+        a["p"] = (a.high.values + a.low.values + a.close.values) / 3
+        a["pivot_range"] = a.high.values - a.low.values
+        a["s1"] = a.p.values * 2 - a.high.values
+        a["s2"] = a.p.values - 1 * a.pivot_range.values
+        a["s3"] = a.p.values - 2 * a.pivot_range.values
+        a["s4"] = a.p.values - 3 * a.pivot_range.values
+        a["r1"] = a.p.values * 2 - a.low.values
+        a["r2"] = a.p.values + 1 * a.pivot_range.values
+        a["r3"] = a.p.values + 2 * a.pivot_range.values
+        a["r4"] = a.p.values + 3 * a.pivot_range.values
+        a.drop(["pivot_range"], axis=1, inplace=True)
+
+    elif method == "demark":
+        conds = (
+            a.close.values == a.open.values,
+            a.close.values > a.open.values,
+        )
+        vals = (
+            a.high.values + a.low.values + a.close.values * 2,
+            a.high.values * 2 + a.low.values + a.close.values,
+        )
+        p = np.select(conds, vals, default=(a.high.values + a.low.values * 2 + a.close.values))
+        a["p"] = p / 4
+        a["s1"] = p / 2 - a.high.values
+        a["r1"] = p / 2 - a.low.values
+    elif method == "camarilla":
+        a["p"] = (a.high.values + a.low.values + a.close.values) / 3
+        a["pivot_range"] = a.high.values - a.low.values
+        a["s1"] = a.close.values - a.pivot_range.values * 1.1 / 12
+        a["s2"] = a.close.values - a.pivot_range.values * 1.1 / 6
+        a["s3"] = a.close.values - a.pivot_range.values * 1.1 / 4
+        a["s4"] = a.close.values - a.pivot_range.values * 1.1 / 2
+        a["r1"] = a.close.values + a.pivot_range.values * 1.1 / 12
+        a["r2"] = a.close.values + a.pivot_range.values * 1.1 / 6
+        a["r3"] = a.close.values + a.pivot_range.values * 1.1 / 4
+        a["r4"] = a.close.values + a.pivot_range.values * 1.1 / 2
+        a.drop(["pivot_range"], axis=1, inplace=True)
+    else:
+        raise ValueError("Invalid method")
+
+    if freq is not anchor:
+        pivots_df = a.reindex(df.index, method="ffill")
+    else:
+        pivots_df = a
+
+    pivots_df.drop(columns=["open", "high", "low", "close"], inplace=True)
+
+    return pivots_df
 
 
-
-PP = Pivot_points(aave)
-
-
-
-aave = dt.aave # Data del día 2 de Junio
-(aave['Close'][aave['Close time'] == np.max(aave['Close time'])])
-
-
-aave.iloc[0:,[1,2,3,4]]
-
-
-
-
-
-
-# VWAP (se calcula con los libros de ordenes)
